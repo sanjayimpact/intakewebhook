b2169a64977b4f7d491b57e1631576eeca84082c
Impactmindz-Tech <impactmindztechsolutions@gmail.com>
2025-07-09 13:51:03 +0530

    update

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import { userrouter } from './Routes/route.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 5000;
+//update
 app.use(cors());
 
 app.use(express.json());
